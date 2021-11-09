4b545150fc50c0369bd217bf355d3225318ba851
Skullking5868 <93954668+Skullking5868@users.noreply.github.com>
2021-11-08 20:22:24 -0400

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+//This is my first project that I had to creat in Java
+public class helloWorld
+{
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+}
